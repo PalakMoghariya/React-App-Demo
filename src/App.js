55dc51b0d57c34b8d89853041ae8a23890549271
Palak Moghariya <palakmoghariya4@gmail.com>
2024-06-06 18:00:19 +0530

    Hii

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    <Navbar title="SIT" aboutText="Contact Us"/>
-    <TextForm heading="Enter Txt to Analyse Below"/>
+    
     </> 
   );
 }
