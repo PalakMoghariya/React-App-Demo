4b457dccbd4d07d9b8148c263477b6ee13926171
Palak Moghariya <palakmoghariya4@gmail.com>
2024-06-07 17:13:55 +0530

    add button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const {text, setText} = useState("Input Your Text");
+    const [text, setText] = useState("Input Your Text");
 
     const handleOnChange = (event) =>{
         // console.log("On Change");
@@ -10,7 +10,7 @@ export default function TextForm(props) {
     }
     const handleUpChange = () =>{
         // console.log("Enter Your Text");
-        var newText = text.toUpperCase();
+        let newText = text.toUpperCase();
         setText(newText);
     }
     const handleLoChange = () =>{
@@ -26,23 +26,39 @@ export default function TextForm(props) {
     const handleClear = () =>{
         setText('');    
     }
+    const handleCopy = () => {
+        let newText = document.getElementById('exampleFormControlTextarea1');
+        newText.select();
+        navigator.clipboard.writeText(newText.value);
+    }
+    const handleRemoveSpace = () =>{
+        let newText = text.split(/[ ] + /);
+        setText(newText.join(" "));
+    }
   return (
     <>
     <div>
     <div className='container my-4'>
       <h1>{props.heading}</h1>
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" value={text} onChange={handleOnChange}></textarea>
-        <button className='my-4 mx-3 btn btn-secondary' onClick={handleUpChange}>Upper Case Letter</button>
-        <button className='my-4 mx-3 btn btn-secondary' onClick={handleLoChange}>Lower Case Letter</button>
-        <button className='my-4 mx-3 btn btn-secondary' onClick={handleSpeak}>Speak</button>
-        <button className='my-4 mx-3 btn btn-secondary' onClick={handleClear}>Clear</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleUpChange}>Upper Case Letter</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleLoChange}>Lower Case Letter</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleSpeak}>Speak</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleClear}>Clear</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleCopy}>Copy Text</button>
+        <button className='my-4 mx-3 btn btn-outline-secondary' onClick={handleRemoveSpace}>Remove Extra Space</button>
+    </div>
+    <div className='container'>
+        <h2>Word Count</h2>
+        <p>{text.split(" ").length} Words</p>
+        <p>{text.length} Characters</p>
+        <h5>{0.005*text.length} Reading Time</h5>
+        <h3>Priview</h3>
+        <p>{text}</p>
     </div>
     </div>
 
-    <div className="container">
-        <h1>Text Summary</h1>
-        <p> Words</p>
-    </div>
+   
     </>
   )
 }
