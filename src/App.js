bc3e3e93b17ecbdf9ab9543acd236c8e1d59e3be
Palak Moghariya <palakmoghariya4@gmail.com>
2024-06-08 17:10:45 +0530

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {useState} from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode] = useState ('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background='black';
+    } else {
+      setMode('light');
+      document.body.style.background='white';
+    }
+  }
   return (
     <>
-      <Navbar title="GIT"/>
+      <Navbar title="GIT" mode={mode} toggleMode={toggleMode}/>
       <TextForm heading = "Enter your Text Here" />
     </>
   );
