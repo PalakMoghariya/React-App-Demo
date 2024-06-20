13a75e95338a7bc658f310faedd1b5a69b25473b
Palak Moghariya <palakmoghariya4@gmail.com>
2024-06-20 17:08:56 +0530

    final new project

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import abc, {b,c,d} from './module2.mjs';
-console.log(abc);
-console.log(b);
-console.log(c);
-console.log(d);
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Palak";
-const b = "Mahi";
-const c = "Divya";
-const d = "Dhruvi";
-
-export default a;
-export{b};
-export{c};
-export{d};
